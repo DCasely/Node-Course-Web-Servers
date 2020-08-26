732e33d57bb1632654fc14eee069ae6dd0c3163c
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-08-26 18:19:50 -0400

    serving up html and json

--- src/app.js
+++ src/app.js
@@ -3,19 +3,31 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('I SENT SOMETHING');
+  res.send('<h1>Weather</h1>');
 });
 
 app.get('/help', (req, res) => {
-  res.send('You are on the HELP Page');
+  res.send([
+    {
+      name: 'Davin',
+      age: 30,
+    },
+    {
+      name: 'Toya',
+      age: 34,
+    },
+  ]);
 });
 
 app.get('/about', (req, res) => {
-  res.send('You are on the ABOUT PAGE');
+  res.send('<h1>About Page</h1>');
 });
 
 app.get('/weather', (req, res) => {
-  res.send('You are on the WEATHER PAGE');
+  res.send({
+    forecast: 'Sunny',
+    location: 'Miami, FL',
+  });
 });
 
 const port = 3000;
