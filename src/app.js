7a7af5d596b67570e464ebe31fb82980242cd141
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-08-27 10:23:30 -0400

    Dynamic Pages with Templating

--- src/app.js
+++ src/app.js
@@ -4,7 +4,29 @@ const express = require('express');
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Weather',
+    name: 'Davin Casely',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Page',
+    name: 'Davin Casely',
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help Page',
+    help_message: 'This is the HELP MESSAGE...',
+  });
+});
 
 app.get('/weather', (req, res) => {
   res.send({
