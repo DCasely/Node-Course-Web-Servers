c9739da2a66e731356de5d8c3c18fc622e6107b6
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-08-30 11:24:13 -0400

    browser http request with fetch

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,26 @@
-console.log('Client side javascript file is loaded!');
+// fetch('http://puzzle.mead.io/puzzle').then((response) => {
+//   response.json().then((data) => {
+//     console.log(data);
+//   });
+// });
+
+// const address = 'miami';
+// const latitude = 25.7743;
+// const longitude = -80.1937;
+
+// const url = `http://api.weatherstack.com/current?access_key=13604b194e0d222bdc14bb1db6712980&query=${encodeURIComponent(
+//   latitude
+// )},${encodeURIComponent(longitude)}&units=f`;
+
+const url = 'http://localhost:3000/weather?address=!';
+
+fetch(url).then((response) => {
+  response.json().then((data) => {
+    if (data.error) {
+      console.log(data.error);
+    } else {
+      console.log(data.location);
+      console.log(data.forecast);
+    }
+  });
+});
