10f87864eb9b5b1011814b719a0c19fd693c2cf0
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-09-01 17:02:57 -0400

    added more weather data to app

--- public/js/app.js
+++ public/js/app.js
@@ -4,6 +4,9 @@ const city = document.querySelector('#city');
 const weather = document.querySelector('#weather');
 const temp = document.querySelector('#temp');
 const feelsLike = document.querySelector('#feelsLike');
+const windSpeed = document.querySelector('#windSpeed');
+const uvIndex = document.querySelector('#uvIndex');
+const visibility = document.querySelector('#visibility');
 
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -17,11 +20,13 @@ weatherForm.addEventListener('submit', (e) => {
       if (data.error) {
         city.textContent = data.error;
       } else {
-        console.log(data);
         city.textContent = data.location;
         weather.textContent = data.forecast.weather;
         temp.textContent = data.forecast.temp;
         feelsLike.textContent = data.forecast.feelsLike;
+        windSpeed.textContent = data.forecast.windSpeed;
+        uvIndex.textContent = data.forecast.uvIndex;
+        visibility.textContent = data.forecast.visibility;
       }
     });
   });
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -6,8 +6,12 @@ const forecast = (latitude, longitude, callback) => {
   )},${encodeURIComponent(longitude)}&units=f`;
 
   request({ url, json: true }, (error, { body }) => {
-    const { temperature: temp, feelslike: feelsLike } = body.current;
     const weather = body.current.weather_descriptions[0];
+    const temp = body.current.temperature;
+    const feelsLike = body.current.feelslike;
+    const windSpeed = body.current.wind_speed;
+    const uvIndex = body.current.uv_index;
+    const visibility = body.current.visibility;
 
     if (error) {
       callback('Unable to connect to weather service!');
@@ -18,6 +22,9 @@ const forecast = (latitude, longitude, callback) => {
         weather,
         temp,
         feelsLike,
+        windSpeed,
+        uvIndex,
+        visibility,
       });
     }
   });
